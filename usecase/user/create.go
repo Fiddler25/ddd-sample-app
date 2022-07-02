dea16cecabee1ae67382b88f9b0ae96bc716e303
Fiddler25 <rager.mka.ti@icloud.com>
2022-07-02 19:00:45 +0900

    Passwordをハッシュ化

--- usecase/user/create.go
+++ usecase/user/create.go
@@ -3,6 +3,7 @@ package user
 import (
 	"github.com/Fiddler25/ddd-sample-app/domain/model"
 	"github.com/Fiddler25/ddd-sample-app/domain/repository/user"
+	"github.com/Fiddler25/ddd-sample-app/sdk"
 	"gorm.io/gorm"
 )
 
@@ -15,5 +16,9 @@ func NewCreateUsecase(db *gorm.DB) CreateUsecase {
 }
 
 func (u CreateUsecase) Execute(data model.User) model.User {
+	rawPassword := data.Password
+	hash := sdk.HashPassword(rawPassword)
+	data.Password = hash
+
 	return user.NewCreateRepository(u.db).Create(data)
 }

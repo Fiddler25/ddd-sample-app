1e89581e6769722e2a7adc03783f22bb432fce63
Fiddler25 <rager.mka.ti@icloud.com>
2022-07-17 10:29:43 +0900

    不要なコードを削除

--- usecase/auth/login.go
+++ usecase/auth/login.go
@@ -3,12 +3,10 @@ package auth
 import (
 	"github.com/Fiddler25/ddd-sample-app/domain/model"
 	"github.com/Fiddler25/ddd-sample-app/domain/repository"
-	"github.com/Fiddler25/ddd-sample-app/domain/vo"
 	"github.com/Fiddler25/ddd-sample-app/sdk/cookie"
 	"github.com/Fiddler25/ddd-sample-app/sdk/password"
 	"github.com/Fiddler25/ddd-sample-app/sdk/session"
 	"github.com/labstack/echo/v4"
-	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
@@ -31,13 +29,14 @@ func (uc LoginUsecase) Execute(c echo.Context, req LoginRequest) *model.User {
 	if !password.IsSame(string(u.Password), req.Password) {
 		return nil
 	}
-	session.Login(c, u.ID)
+
 	var oldSessionID session.ID
 	if ck, err := c.Cookie(cookie.CookieNameSession); err != nil {
 		oldSessionID = ""
 	} else {
 		oldSessionID = session.ID(ck.Value)
 	}
+
 	sRepo := session.NewRepository(uc.db)
 	if oldSessionID != "" {
 		if oldSession, err := sRepo.Find(oldSessionID); err != nil {
@@ -50,26 +49,8 @@ func (uc LoginUsecase) Execute(c echo.Context, req LoginRequest) *model.User {
 	sessionID := session.NewID()
 	sess := session.NewModel(sessionID, u.ID)
 	sRepo.Create(sess)
+
 	cookie.Set(c, string(sess.SessionID))
 
-	token := vo.NewRandomToken()
-	u = updateRememberDigest(u, repo, token)
-
-	if !isAuthenticated(u.RememberDigest, token) {
-		return nil
-	}
-
 	return &u
-}
-
-func updateRememberDigest(user model.User, repo repository.UserRepository, token vo.Token) model.User {
-	user.SetRememberDigest(token)
-	repo.UpdateRememberDigest(&user)
-
-	return user
-}
-
-func isAuthenticated(digest, token vo.Token) bool {
-	err := bcrypt.CompareHashAndPassword([]byte(digest), []byte(token))
-	return err == nil
 }

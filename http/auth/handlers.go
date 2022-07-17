1e82f818ca22db4ebf085b438c3ba1209a8a52fb
Fiddler25 <rager.mka.ti@icloud.com>
2022-07-17 10:39:32 +0900

    セッション関連処理をhandlersに移動

--- http/auth/handlers.go
+++ http/auth/handlers.go
@@ -1,7 +1,10 @@
 package auth
 
 import (
+	"github.com/Fiddler25/ddd-sample-app/domain/model"
 	"github.com/Fiddler25/ddd-sample-app/gorm"
+	"github.com/Fiddler25/ddd-sample-app/sdk/cookie"
+	"github.com/Fiddler25/ddd-sample-app/sdk/session"
 	"github.com/Fiddler25/ddd-sample-app/usecase/auth"
 	"github.com/labstack/echo/v4"
 	"net/http"
@@ -18,5 +21,35 @@ func Login(c echo.Context) error {
 		return c.JSON(http.StatusUnauthorized, "ログインできません。")
 	}
 
+	if err := startSession(c, res.ID); err != nil {
+		return c.JSON(http.StatusUnauthorized, "ログインできません。")
+	}
+
 	return c.JSON(http.StatusOK, res)
+}
+
+func startSession(c echo.Context, userID model.UserID) error {
+	var oldSessionID session.ID
+	if ck, err := c.Cookie(cookie.CookieNameSession); err != nil {
+		oldSessionID = ""
+	} else {
+		oldSessionID = session.ID(ck.Value)
+	}
+
+	sRepo := session.NewRepository(gorm.DB())
+	if oldSessionID != "" {
+		if oldSession, err := sRepo.Find(oldSessionID); err != nil {
+			return err
+		} else {
+			sRepo.Delete(oldSession)
+		}
+	}
+
+	sessionID := session.NewID()
+	sess := session.NewModel(sessionID, userID)
+	sRepo.Create(sess)
+
+	cookie.Set(c, string(sess.SessionID))
+
+	return nil
 }
--- usecase/auth/login.go
+++ usecase/auth/login.go
@@ -3,9 +3,7 @@ package auth
 import (
 	"github.com/Fiddler25/ddd-sample-app/domain/model"
 	"github.com/Fiddler25/ddd-sample-app/domain/repository"
-	"github.com/Fiddler25/ddd-sample-app/sdk/cookie"
 	"github.com/Fiddler25/ddd-sample-app/sdk/password"
-	"github.com/Fiddler25/ddd-sample-app/sdk/session"
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
 )
@@ -29,28 +27,6 @@ func (uc LoginUsecase) Execute(c echo.Context, req LoginRequest) *model.User {
 	if !password.IsSame(string(u.Password), req.Password) {
 		return nil
 	}
-
-	var oldSessionID session.ID
-	if ck, err := c.Cookie(cookie.CookieNameSession); err != nil {
-		oldSessionID = ""
-	} else {
-		oldSessionID = session.ID(ck.Value)
-	}
-
-	sRepo := session.NewRepository(uc.db)
-	if oldSessionID != "" {
-		if oldSession, err := sRepo.Find(oldSessionID); err != nil {
-			return nil
-		} else {
-			sRepo.Delete(oldSession)
-		}
-	}
-
-	sessionID := session.NewID()
-	sess := session.NewModel(sessionID, u.ID)
-	sRepo.Create(sess)
-
-	cookie.Set(c, string(sess.SessionID))
 
 	return &u
 }
